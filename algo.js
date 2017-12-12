1f0dfecc4933e6db7b8ff35d9f0a8b7cc1e1e7ae
Mark Pare <mark.p.pare@gmail.com>
2017-12-11 19:32:19 -0800

    updated stuff

---
+++ algo.js
@@ -0,0 +1,10 @@
+var num = 10010;
+
+var word = "Mark";
+// word[0] returns "M"
+
+var numAsString = num.toString(); // "10010"
+
+console.log(numAsString);
+
+console.log('Type is: ', typeof numAsString);
